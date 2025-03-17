bf50027818815c53d0be247d6a3de2042e0b2587
admin <kms09114@gmail.com>
2025-03-17 22:40:48 +0200

    add read token

--- src/test/java/org/universeapps/Tests.java
+++ src/test/java/org/universeapps/Tests.java
@@ -1,10 +1,11 @@
 package org.universeapps;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SNAKE_CASE;
 import static io.restassured.RestAssured.*;
@@ -18,28 +19,30 @@ public class Tests {
     private static final ObjectMapper objectMapper = new ObjectMapper()
             .setPropertyNamingStrategy(SNAKE_CASE);
 
-    private String getBearerToken() {
-        return "";
+    private String getBearerToken() throws IOException {
+        try (FileInputStream file = new FileInputStream("token.txt")) {
+            return new String(file.readAllBytes());
+        }
     }
 
 
-    private ValidatableResponse executeRequest(RequestBody requestBody){
+    private ValidatableResponse executeRequest(RequestBody requestBody) {
         try {
             return given()
-                    .header("Authorization",  "Bearer " + getBearerToken())
+                    .header("Authorization", "Bearer " + getBearerToken())
                     .contentType(ContentType.JSON)
                     .body(objectMapper.writeValueAsString(requestBody))
                     .when()
-                    .post(BASE_URL+ENDPOINT)
+                    .post(BASE_URL + ENDPOINT)
                     .then()
                     .assertThat();
-        } catch (JsonProcessingException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @Test
-    public void firstTest(){
+    public void firstTest() {
         executeRequest(RequestBody.withDefault())
 //                .statusCode(200)
                 .body(startsWith("data"));

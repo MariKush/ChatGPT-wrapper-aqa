9c811ab5e0365011d6da47dd3457d3b7256c63a6
admin <kms09114@gmail.com>
2025-03-17 23:29:18 +0200

    add positiveTest

--- src/test/java/org/universeapps/Tests.java
+++ src/test/java/org/universeapps/Tests.java
@@ -2,14 +2,18 @@ package org.universeapps;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.ContentType;
-import io.restassured.response.ValidatableResponse;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SNAKE_CASE;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class Tests {
@@ -26,25 +30,30 @@ public class Tests {
     }
 
 
-    private ValidatableResponse executeRequest(RequestBody requestBody) {
+    private Response executeRequest(RequestBody requestBody) {
         try {
             return given()
                     .header("Authorization", "Bearer " + getBearerToken())
                     .contentType(ContentType.JSON)
                     .body(objectMapper.writeValueAsString(requestBody))
                     .when()
-                    .post(BASE_URL + ENDPOINT)
-                    .then()
-                    .assertThat();
+                    .post(BASE_URL + ENDPOINT);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @Test
-    public void firstTest() {
-        executeRequest(RequestBody.withDefault())
-//                .statusCode(200)
-                .body(startsWith("data"));
+    public void positiveTest() {
+        Response response = executeRequest(RequestBody.withDefault());
+        JsonPath responseJsonPath = response.then()
+                .statusCode(200)
+                .extract().jsonPath();
+
+        assertAll(
+                () -> assertThat(responseJsonPath.get("id"), matchesPattern("^chatcmpl-[A-Za-z0-9]{29}$")),
+                () -> assertEquals("chat.completion", responseJsonPath.get("object")),
+                () -> assertThat(responseJsonPath.get("choices[0].message.content"), startsWith("I can't provide"))
+        );
     }
 }
